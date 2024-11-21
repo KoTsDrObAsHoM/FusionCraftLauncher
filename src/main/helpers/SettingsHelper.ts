cad64fc675ff69ae7783ef2818894a50c57d9893
KoTsDrObAsHoM <86474772+KoTsDrObAsHoM@users.noreply.github.com>
2024-11-21 15:31:26 +0300

    Initialize dev

---
+++ src/main/helpers/SettingsHelper.ts
@@ -0,0 +1,57 @@
+import os from 'os';
+import Store from 'electron-store';
+import { app } from 'electron';
+import { parse } from 'path';
+
+import { SettingsFormat } from '../../common/types';
+import { StorageHelper } from './StorageHelper';
+
+export class SettingsHelper {
+    static store: any;
+
+    static init() {
+        this.store = new Store({
+            cwd: parse(app.getPath('exe')).dir,
+            defaults: {
+                client: this.defaultsValue(),
+            },
+        });
+    }
+
+    static defaultsValue(): SettingsFormat {
+        return {
+            token: '0',
+            dir: StorageHelper.storageDir,
+            autoConnect: false,
+            fullScreen: false,
+            memory: 1024,
+            startDebug: false,
+        };
+    }
+
+    static getStore() {
+        return this.store;
+    }
+
+    static getAllFields(): SettingsFormat {
+        return this.getStore().get('client');
+    }
+
+    static getField(name: string) {
+        return this.getStore().get('client.' + name);
+    }
+
+    static setField(field: string, value: string | boolean | number): void {
+        return this.getStore().set('client.' + field, value);
+    }
+
+    static getTotalMemory(): number {
+        const remainingMemMegabytes = Math.floor(os.totalmem() / 1024 ** 2) / 2;
+
+        return (
+            remainingMemMegabytes -
+            (remainingMemMegabytes % 1024) +
+            (remainingMemMegabytes % 1024 ? 1024 : 0)
+        );
+    }
+}
---
+++ src/main/helpers/StorageHelper.ts
@@ -0,0 +1,68 @@
+import { cpSync, existsSync, mkdirSync, rmSync } from 'fs';
+import { homedir } from 'os';
+import { resolve } from 'path';
+
+import { appPath } from '@config';
+import { app } from 'electron';
+
+import { LogHelper } from '../helpers/LogHelper';
+import { PlatformHelper } from './PlatformHelper';
+import { SettingsHelper } from './SettingsHelper';
+
+export class StorageHelper {
+    static storageDir: string = this.getPlatformStorageDir();
+    static assetsDir: string = resolve(this.storageDir, 'assets');
+    static clientsDir: string = resolve(this.storageDir, 'clients');
+    static librariesDir: string = resolve(
+        this.storageDir,
+        'libraries',
+    );
+    static javaDir: string = resolve(this.storageDir, 'java');
+    static logFile: string = resolve(this.storageDir, 'Launcher.log');
+
+    static createMissing(): void {
+        if (!existsSync(this.storageDir)) mkdirSync(this.storageDir);
+        if (
+            SettingsHelper.getStore().get('client.dir') &&
+            SettingsHelper.getStore().get('client.dir') !== this.storageDir
+        ) {
+            this.assetsDir = resolve(SettingsHelper.getStore().get('client.dir'), 'assets');
+            this.librariesDir = resolve(
+                SettingsHelper.getStore().get('client.dir'),
+                'libraries',
+            );
+            this.clientsDir = resolve(SettingsHelper.getStore().get('client.dir'), 'clients');
+            this.javaDir = resolve(SettingsHelper.getStore().get('client.dir'), 'java');
+            this.logFile = resolve(SettingsHelper.getStore().get('client.dir'), 'Launcher.log');
+        } else {
+            if (!existsSync(this.assetsDir)) mkdirSync(this.assetsDir);
+            if (!existsSync(this.clientsDir)) mkdirSync(this.clientsDir);
+            if (!existsSync(this.librariesDir)) mkdirSync(this.librariesDir);
+            if (!existsSync(this.javaDir)) mkdirSync(this.javaDir);
+        }
+    }
+
+    private static getPlatformStorageDir() {
+        if (PlatformHelper.isMac) {
+            return resolve(app.getPath('userData'), '../', appPath);
+        }
+        return resolve(homedir(), appPath);
+    }
+
+    static migration(path: string) {
+        cpSync(this.storageDir, path, { recursive: true });
+        rmSync(this.assetsDir, { recursive: true });
+        rmSync(this.clientsDir, { recursive: true });
+        rmSync(this.librariesDir, { recursive: true });
+        rmSync(this.javaDir, { recursive: true });
+        this.storageDir = path;
+        this.assetsDir = resolve(this.storageDir, 'assets');
+        this.clientsDir = resolve(this.storageDir, 'clients');
+        this.librariesDir = resolve(this.storageDir, 'libraries');
+        this.javaDir = resolve(this.storageDir, 'java');
+        this.logFile = resolve(this.storageDir, 'Launcher.log');
+
+        SettingsHelper.setField('dir', path);
+        LogHelper.info('Migration completed successfully');
+    }
+}
