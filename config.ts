a6eb2078ece604f13eb81b633be63e7dc4ed320a
TheIhori <86474772+KoTsDrObAsHoM@users.noreply.github.com>
2024-11-21 19:36:50 +0300

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
-import publicKey from './public.pem?raw';
+import token from './public.pem';
 
 export const window = {
     width: 900,
@@ -7,59 +7,13 @@ export const window = {
     resizable: false,
     maximizable: false,
     fullscreenable: false,
-    title: 'Aurora Launcher',
+    title: 'Fusion Craft Launcher',
 };
 
 export const api = {
     ws: 'ws://127.0.0.1:1370/ws',
     web: 'http://127.0.0.1:1370',
-    publicKey,
+    // extraToken: token,
 };
 
-export const appPath = '.aurora-launcher';
-
-export const discordRPC = {
-    appId: '1214685301793103902',
-    default: {
-        firstLineText: 'Тестирую лаунчер',
-        secondLineText: 'Чувак, ты думал здесь что-то будет?',
-        buttons: [
-            {
-                label: 'Прекол',
-                url: 'https://youtu.be/dQw4w9WgXcQ',
-            },
-        ],
-        largeImageKey: 'logo',
-        smallImageKey: 'logo_mc',
-        largeImageText: 'Aurora Launcher',
-        smallImageText: 'Minecraft',
-    },
-    profile: {
-        firstLineText: 'Выбираю тестируемый профиль игры',
-        secondLineText: 'Загружаю {server}',
-        buttons: [
-            {
-                label: 'Прекол',
-                url: 'https://youtu.be/dQw4w9WgXcQ',
-            },
-        ],
-        largeImageKey: 'logo',
-        smallImageKey: 'logo_mc',
-        largeImageText: 'Aurora Launcher',
-        smallImageText: 'Minecraft',
-    },
-    game: {
-        firstLineText: 'Играю на тестовом сервере',
-        secondLineText: 'Играю за {nickname}',
-        buttons: [
-            {
-                label: 'Прекол',
-                url: 'https://youtu.be/dQw4w9WgXcQ',
-            },
-        ],
-        largeImageKey: 'logo',
-        smallImageKey: 'logo_mc',
-        largeImageText: 'Aurora Launcher',
-        smallImageText: 'Minecraft',
-    },
-};
+export const appPath = '.fusioncraft';
